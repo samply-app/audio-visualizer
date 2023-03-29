8e0996fd92cb4a8c97c1f081d14d3aaa437f6f21
Kevyn <kevyn@samplyaudio.com>
2023-03-29 16:52:05 -0500

    cool portal ray spawns

--- src/visualizers/portal.ts
+++ src/visualizers/portal.ts
@@ -31,9 +31,8 @@ function frameHandler(
   const { width, height } = ctx.canvas;
   const center: Vec2 = { x: ctx.canvas.width / 2, y: ctx.canvas.height / 2 };
   const minRadius = 50;
-  const corePortalArray: Vec2[] = [];
-  const velocityScalar = 3;
-  const maxRayLength = 500;
+  const velocityScalar = 1;
+  const maxRayLength = Math.min(width, height);
 
   /** Draw centered outlined points */
   function drawDebugPoint(point: Vec2, radius = 4, color = "aqua") {
@@ -59,8 +58,6 @@ function frameHandler(
     curveColor = "cyan",
     curveFill?: string
   ) {
-    const pointRadius = 4;
-    ctx.lineWidth = 4;
     // Draw bezier curve
     ctx.strokeStyle = curveColor;
     ctx.beginPath();
@@ -70,7 +67,8 @@ function frameHandler(
       ctx.fillStyle = curveFill;
       ctx.fill();
     }
-    ctx.lineWidth = 10;
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = curveFill;
     ctx.stroke();
     // Draw control points
     // ctx.strokeStyle = "orange";
@@ -101,20 +99,6 @@ function frameHandler(
     return { r, theta };
   }
 
-  /** Translate a vector to canvas center */
-  function translateToCenter(vec: Vec2): Vec2 {
-    vec.x += center.x;
-    vec.y += center.y;
-    return vec;
-  }
-
-  /** Translate a vector from canvas center */
-  function translateFromCenter(vec: Vec2): Vec2 {
-    vec.x -= center.x;
-    vec.y -= center.y;
-    return vec;
-  }
-
   /** Get the angle in radians of the specified radial index  */
   function getRadialAngleRadians(
     radial: number,
@@ -124,72 +108,14 @@ function frameHandler(
     return (radial * arcLength) / count;
   }
 
-  /** Get the angle in radians of the specified radial index  */
-  function getTweenPoints(): Vec2[] {
-    const tweenShift = (Math.PI * 2) / corePortalArray.length / 2;
-    return Array.from({ length: corePortalArray.length }).map((_, i) =>
-      translateToCenter(
-        polarToCartesian({
-          r: minRadius,
-          theta: (i * Math.PI * 2) / corePortalArray.length + tweenShift
-        })
-      )
-    );
-  }
-
-  /** Create a single radial point */
-  function plotPortalRadial(radialMag: number, thetaRads: number) {
-    // get polar coordinates
-    const radialVec = polarToCartesian({ r: radialMag, theta: thetaRads });
-
-    // transform to center
-    radialVec.x += center.x;
-    radialVec.y += center.y;
-
-    // KLS DEBUG Draw dots to map out shape
-    // ctx.fillRect(radialVec.x, radialVec.y, 10, 10);
-
-    return radialVec;
-  }
-
-  /** Rotate a set of vectors about the  */
-  function rotate2d(
-    input: Vec2[],
-    rads: number,
-    aboutCenter: Vec2 = center
-  ): Vec2[] {
-    return input.map(vec => {
-      // translate from center
-      const rawVec = translateFromCenter(vec);
-
-      // convert input to polar
-      const polar = cartesianToPolar(rawVec);
-
-      // rotate
-      polar.theta += rads;
-      return translateToCenter(polarToCartesian(polar));
-    });
-  }
-
-  // mirror the frequency array
+  // grab some frequencies and mirror them
   const frequencies = Array.from(frequency.slice(0, radialCount / 2));
   frequencies.push(...frequencies.slice().reverse());
-
-  // calculate radial velocity for each frequency
-  frequencies.forEach((f, idx) => {
-    const velocity: number = (f - lastFreqMags[idx]) / deltaTime;
-    if (
-      (!rays[idx]?.velocity && velocity > 0) ||
-      velocity > rays[idx]?.velocity
-    ) {
-      rays[idx].velocity = velocity;
-    }
-  });
 
   /** Draw a ray given polar coordinates */
   function drawRay(point: Polar2, strokeColor = "cyan", fillColor = "cyan") {
     // Create control points on either side of vector
-    const angularPadding = Math.PI / 8;
+    const angularPadding = Math.PI / 16;
     let rayA_polar: Polar2 = {
       r: point.r,
       theta: point.theta - angularPadding
@@ -198,110 +124,86 @@ function frameHandler(
       r: point.r,
       theta: point.theta + angularPadding
     };
-    if (point.r > 1200) {
-      console.log(
-        `kls point.theta: ${point.theta}, rayA_polar: ${rayA_polar.theta}, rayB_polar: ${rayB_polar.theta}`
-      );
-    }
     const rayA_cart: Vec2 = polarToCartesian(rayA_polar);
     const rayB_cart: Vec2 = polarToCartesian(rayB_polar);
 
     // Draw the ray
-    drawBezier(center, rayA_cart, rayB_cart, center, strokeColor, fillColor);
-
-    // KLS DEBUG draw control points
-    drawDebugPoint(rayA_cart, 8, "red");
-    drawDebugPoint(rayB_cart, 8, "orange");
-    drawDebugPoint(polarToCartesian(point), 4, "cyan");
-  }
-
-  for (let i = 1; i <= radialCount; i += 1) {
-    let valleyPosition = {
-      x: 0.5 * width,
-      y: (1 / (radialCount - (i - 1))) * height + frequency[i] * 2
-    };
-
-    let gradient = ctx.createLinearGradient(0, 0, width, 0); // horizontal gradient
-
-    // const luma = `${((i / radialCount) * 50).toFixed(0)}%`;
-    const luma = "50%";
-
-    gradient.addColorStop(0, `hsl(0, 50%, ${luma})`);
-    gradient.addColorStop(valleyPosition.x / width, `hsl(0, 50%, ${luma})`);
-    gradient.addColorStop(1, `hsl(0, 50%, ${luma})`);
-
-    corePortalArray.push(
-      plotPortalRadial(
-        minRadius + frequencies[i] * velocityScalar,
-        getRadialAngleRadians(i)
-      )
+    drawBezier(
+      { x: 0, y: 0 },
+      rayA_cart,
+      rayB_cart,
+      { x: 0, y: 0 },
+      strokeColor,
+      fillColor
     );
 
-    ctx.fillStyle = gradient;
-    ctx.fill();
+    // KLS DEBUG draw control points
+    // drawDebugPoint(rayA_cart, 8, "red");
+    // drawDebugPoint(rayB_cart, 8, "orange");
+    // drawDebugPoint(polarToCartesian(point), 4, "cyan");
   }
 
-  // get the tweening points
-  const tweenPoints: Vec2[] = getTweenPoints();
-  const drawPoints = rotate2d(corePortalArray, Math.PI / 2);
+  const corePortalArray: Polar2[] = Array.from(
+    { length: radialCount },
+    (_, idx) => ({
+      r: minRadius + frequencies[idx] * velocityScalar,
+      theta: getRadialAngleRadians(idx)
+    })
+  );
+
+  // transform context to center
+  ctx.translate(center.x, center.y);
 
   // Draw the core portal array
   ctx.beginPath();
-  for (let i = 0; i < radialCount; i++) {
-    drawBezier(
-      center,
-      drawPoints[i % radialCount],
-      drawPoints[(i + 1) % radialCount],
-      center,
-      `hsl(${(i * 360) / radialCount}, 200%, 0%)`,
-      `hsl(${(i * 360) / radialCount}, 50%, 50%)`
-    );
+
+  function radialColorByIndex(
+    idx: number,
+    saturation = 50,
+    lightness = 50,
+    alpha = 1
+  ) {
+    return `hsla(${
+      (idx * 360) / radialCount
+    }, ${saturation}%, ${lightness}%, ${alpha})`;
   }
-  // drawPoints.forEach((point, i) => {
-  //   drawRay(cartesianToPolar(point), "white", "white");
-  // });
-  ctx.closePath();
 
-  // Draw rays
-  ctx.beginPath();
-  rays.forEach((ray, i) => {
-    if (!ray.point && !ray.velocity) {
-      ray.point = cartesianToPolar(drawPoints[i]);
-    } else {
-      // ray.point.r += ray.velocity * deltaTime;
+  // calculate radial velocity for each frequency
+  frequencies.forEach((f, idx) => {
+    const velocity: number = (f - lastFreqMags[idx]) / deltaTime;
+    if (velocity > rays[idx]?.velocity) {
+      rays[idx].velocity = velocity;
+      rays[idx].point = corePortalArray[idx];
     }
-    // convert back to cartesian
-    // const rayCart = translateToCenter(
-    //   polarToCartesian(rays[i % radialCount].point)
-    // );
-    // drawBezier(
-    //   center,
-    //   rays[i % radialCount].point,
-    //   rays[(i + 1) % radialCount].point,
-    //   center,
-    //   "white",
-    //   "white"
-    // `hsl(${(i * 360) / radialCount}, 200%, 0%)`,
-    // `hsl(${(i * 360) / radialCount}, 50%, 50%)`
-    // );
-
-    // reset extended rays
-    // if (ray.point.r > maxRayLength) {
-    //   ray.point = { r: 0, theta: 0 };
-    //   ray.velocity = 0;
-    // }
   });
+
+  rays.forEach((ray, idx) => {
+    if (ray.point?.r && ray.point?.r > maxRayLength) {
+      ray.point = undefined;
+      ray.velocity = 0;
+    }
+    // if ray is active, draw it
+    if (ray.point && ray.velocity > 0) {
+      // draw the ray
+      drawRay(
+        ray.point,
+        "#000000",
+        radialColorByIndex(idx, 50, 50, 1 - ray.point.r / maxRayLength)
+      );
+      ray.point.r += ray.velocity;
+    }
+  });
+
+  corePortalArray.forEach((point, idx) => {
+    drawRay(
+      point,
+      `hsl(${(idx * 360) / radialCount}, 200%, 0%)`,
+      radialColorByIndex(idx)
+    );
+  });
+
   ctx.closePath();
-
-  // // DEBUG draw tween points
-  // for (let i = 0; i < tweenPoints.length; i++) {
-  //   drawDebugPoint(tweenPoints[i], 4, i === 1 ? "red" : undefined);
-  // }
-
-  // // DEBUG draw core portal points
-  // for (let i = 0; i < corePortalArray.length; i++) {
-  //   drawDebugPoint(corePortalArray[i], 6, i === 1 ? "white" : "green");
-  // }
+  ctx.resetTransform();
 
   // set the last frequencies
   lastFreqMags = new Uint8Array(frequencies);
