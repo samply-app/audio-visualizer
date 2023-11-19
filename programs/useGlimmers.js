f40d97160668e069689946445317050b992610a5
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-11-18 17:38:15 -0700

    basic fib

--- programs/useGlimmers.js
+++ programs/useGlimmers.js
@@ -1,6 +1,25 @@
+function fibonacciSpiralPoint(index, spacing = 10, radius = 2) {
+  if (index < 0) {
+    throw new Error("Index must be a non-negative integer");
+  }
+
+  if (index === 0) {
+    return { x: 0, y: 0 };
+  }
+
+  let goldenRatio = (1 + Math.sqrt(5)) / 2;
+  let angle = index * (2 * Math.PI / goldenRatio);
+  let x = radius * Math.sqrt(index) * Math.cos(angle) * spacing;
+  let y = radius * Math.sqrt(index) * Math.sin(angle) * spacing;
+
+  return { x, y };
+}
+
 function createGlimmer() {
+  
   const colorLow = { r: 0, g: 43, b: 65, a: 0 }
   const colorHigh = { r: 186, g: 230, b: 253, a: 1 }
+  const radius = 1; // max radius of glimmer
   
   let time = 0; // time since first update (frames)
   let x = 0;
@@ -42,7 +61,8 @@ function createGlimmer() {
 
   function update() {
     time += 1;
-    energy = time / 100;
+    // energy = time / 100;
+    energy = 1;
   }
 
   function reset(){
@@ -54,7 +74,7 @@ function createGlimmer() {
   function draw(ctx) {
     ctx.fillStyle = getColor(0.5);
     ctx.beginPath();
-    ctx.arc(x, y, lerp(0, 8, time / 100), 0, 2 * Math.PI);
+    ctx.arc(x, y, lerp(0, 8, energy * radius), 0, 2 * Math.PI);
     ctx.fill();
 
     if(time > 100) reset();
@@ -78,18 +98,28 @@ export default function useGlimmers() {
 
   function drawFrame(ctx, width, height) {    
 
-    glimmer1.setPosition(width / 3, height / 2);
-    glimmer2.setPosition(width / 2, height / 2);
-    glimmer3.setPosition(2 * width / 3, height / 2);
+    const origin = { x: width / 2, y: height / 2}
+
+    // const { x, y } = fibonacciSpiralDotPosition(origin.x, origin.y + 20, 0);
+    // const { x2, y2 } = fibonacciSpiralDotPosition(origin.x, origin.y - 100, 0);
+    // const { x3, y3 } = fibonacciSpiralDotPosition(origin.x + 20, origin.y, 0);
+
+    // glimmer1.setPosition(200, 100);
+    const { x: xFib1, y: yFib1 } = fibonacciSpiralPoint(0);
+    glimmer1.setPosition(origin.x + xFib1, origin.y + yFib1);
+    // glimmer3.setPosition(500, 500);
 
     glimmer1.draw(ctx);
     glimmer1.update();
 
-
+    const { x: xFib2, y: yFib2 } = fibonacciSpiralPoint(1);
+    glimmer2.setPosition(origin.x + xFib2, origin.y + yFib2);
     glimmer2.draw(ctx);
     glimmer2.update();    
 
     
+    const { x: xFib3, y: yFib3 } = fibonacciSpiralPoint(2);
+    glimmer3.setPosition(origin.x + xFib3, origin.y + yFib3);
     glimmer3.draw(ctx);
     glimmer3.update();
 
